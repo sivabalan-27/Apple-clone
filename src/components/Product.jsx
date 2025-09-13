7b36c1ad34f2e047af2add8460d7d59dfa59ce62
sivabalan <siva270410@gmail.com>
2025-09-14 04:41:48 +0530

    Update Product.jsx

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,3 +1,4 @@
+import iphones from "iphone_17_colours.webp"
 function product(){
     return(
         <div className="mt-5 text-center bg-gray-50">
@@ -8,7 +9,7 @@ function product(){
             <button className="text-white bg-blue-600 w-28 py-2 px-3 rounded-3xl hover:bg-blue-700">Learn More</button>
             <button className="text-blue-600 w-28 py-2 rounded-3xl border-2 border-blue-400 hover:bg-blue-600 hover:text-white">Pre-Order</button>
             </div>
-            <img src="src\images\iphone_17_colours.webp" alt="" className="inline-block" />
+            <img src={iphones} alt="" className="inline-block" />
         </div>
     )
 }
